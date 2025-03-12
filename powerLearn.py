20ae19e293c9107491b81d2d27da6837277a3443
Alvyn <165035961+dlakk@users.noreply.github.com>
2025-03-12 23:06:21 +0300

    added python calculator test

---
+++ powerLearn.py
@@ -0,0 +1,11 @@
+def greeting():
+  name = str(input("What is your name? "))
+  color = str(input("What is your favorite color? "))
+  return "Hello, " + name + " Your favorite color is " + color + "!"
+
+print(greeting())
+
+
+#Create a multiple-choice quiz with questions about Python, movies, or any fun topic! Display scores at the end and allow the user to play again.
+
+# Build a program that stores a list of jokes and randomly selects one to display every time the user runs it. Add a fun twist with jokes about Python or programming! 🐍💡
