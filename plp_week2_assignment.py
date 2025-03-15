dad3557e5b6366cc69cdb86755e6db111e376062
dlakk <otiilvin@gmail.com>
2025-03-15 07:16:06 +0300

    added week2 assignment on list

---
+++ plp_week2_assignment.py
@@ -0,0 +1,25 @@
+# Write a Python program that does the following:
+
+# Create an empty list called my_list.
+my_list = []
+
+# Append the following elements to my_list: 10, 20, 30, 40.
+for item in [10, 20, 30, 40]:
+    my_list.append(item)
+
+# Insert the value 15 at the second position in the list.
+my_list.insert(1, 15)
+
+# Extend my_list with another list: [50, 60, 70].
+my_list.extend([50,60,70])
+
+# Remove the last element from my_list.
+my_list.pop()
+
+# Sort my_list in ascending order.
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list.
+print(my_list.index(30))
+
+print(my_list)
